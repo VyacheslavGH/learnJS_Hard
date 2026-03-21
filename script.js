d496326eb2990150272eecba5311c7c5cf534002
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-21 10:12:16 +0300

    Выполнил ДЗ 4 урока

--- script.js
+++ script.js
@@ -5,21 +5,14 @@ const removeSpacesSetMaxLength = function (str) {
     let strElipsis = "";
     let result = "";
 
-    // Работает
     // Проверка на корректно введенный тип данных
     if (typeof str !== "string") {
-        alert("Введите строкове значение");
-        return console.log("Пользователь ввел другой тип данных");
+        return "Пользователь ввел другой тип данных";
     }
 
     // Убираем пробелы в начале и конце строки, если они есть
-    // Функция убирает лишь один пробел, после чего делает return
-    // Предполагаю это потому, что ретерн стоит внутри while
-    // но если вынести его за while, цикл уходит в бесконечность
-    // нужно подумать над решением
     const removeSpaces = function (strToClean) {
         let strToArr = strToClean.split("");
-        console.log(strToArr);
 
         if (strToArr[0] !== " " && strToArr[strToArr.length - 1] !== " ") {
             return strToClean;
@@ -28,29 +21,43 @@ const removeSpacesSetMaxLength = function (str) {
         while (strToArr[0] === " " || strToArr[strToArr.length - 1] === " ") {
             if (strToArr[0] === " ") {
                 strToArr.splice(0, 1);
-                console.log(strToArr);
             } else if (strToArr[strToArr.length - 1] === " ") {
                 strToArr.splice(-1, 1);
-                console.log(strToArr);
             }
         }
 
-        console.log(`Возврат из функции удаления пробелов = ${strToArr.join("")}`);
         return strToArr.join("");
     };
 
-    strWithoutSpaces = removeSpaces(str);
-
-    // ДОбавляем троеточие, если длина строки более 30 символов
+    // Добавляем троеточие, если длина строки более 30 символов
     const ellipsis = function (strMaxLength) {
         if (strMaxLength.length <= 30) {
             return strMaxLength;
         } else {
-            return (strElipsis = strMaxLength.slice(0, 30) + "...");
+            return (strElipsis = strMaxLength.slice(0, 31) + "...");
         }
     };
 
+    strWithoutSpaces = removeSpaces(str);
     result = ellipsis(strWithoutSpaces);
 
     return result;
 };
+
+// Логи для проверки
+
+// Проверка на ввод нужного типа данных
+console.log(removeSpacesSetMaxLength(12));
+console.log("\n");
+
+// Убираем лишние пробелы по краям строки
+console.log(removeSpacesSetMaxLength("  gjkgj           "));
+console.log("\n");
+
+// Если строка более 30 символов, то добавляем троеточие
+console.log(removeSpacesSetMaxLength("jhfgljhfdlgjhldfkjghldfkjhggffg"));
+console.log(removeSpacesSetMaxLength("jhfgljhfdlgjhldfkjghldfkjhggffg").length - 3);
+console.log("\n");
+
+// Иначе ничего не делаем и просто выводим строку
+console.log(removeSpacesSetMaxLength("Привет, Мир!"));
