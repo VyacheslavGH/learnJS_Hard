eaf5d6f3d6eed6a3ac82d6560283bc6f996940b4
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-21 19:36:38 +0300

    решение первого задания

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 "use script";
+
+let inputNum
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num) && isFinite(num));
+};
+
+const getAnswer = function () {
+    while (isNumber(inputNum = prompt("Введите любое число", 100);)) {
+        
+    }
+}
