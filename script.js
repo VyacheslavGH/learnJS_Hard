5eae2c9cafa593af0b40a0168cf27f5ba278aa86
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-22 12:57:43 +0300

    Выполнил ДЗ 5 урока 1 часть

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 "use script";
 
-let inputNum
+let inputNum;
 
+// Проверяем, явлвяется ли переданное значение числом
 const isNumber = function (num) {
     return !isNaN(parseFloat(num) && isFinite(num));
 };
 
-const getAnswer = function () {
-    while (isNumber(inputNum = prompt("Введите любое число", 100);)) {
-        
+const getNumber = function () {
+    let arr = [];
+
+    while (!isNumber((inputNum = prompt("Введите любое число", 100)))) {
+        if (inputNum === null) {
+            return "Пользователь отменил ввод";
+        }
+
+        inputNum = prompt("Введите любое число", 100);
     }
-}
+
+    arr = inputNum.split("");
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === " ") {
+            arr.splice(i, 1);
+            i = i - 1;
+        }
+    }
+
+    return Number((inputNum = arr.join("")));
+};
+
+console.log(getNumber());
