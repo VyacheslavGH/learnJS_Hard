261f50626f05efb6bb392840ec015b0f9f7141f4
Vyacheslav <slavaslava.s@yandex.ru>
2026-04-06 22:05:31 +0300

    В процессе выполнения урок 7 хард версия

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
 "use script";
 
-let inputNum;
+const week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 
-// Проверяем, явлвяется ли переданное значение числом
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num) && isFinite(num));
-};
-
-const getNumber = function () {
-    let arr = [];
-
-    while (!isNumber((inputNum = prompt("Введите любое число", 100)))) {
-        if (inputNum === null) {
-            return "Пользователь отменил ввод";
-        }
-
-        inputNum = prompt("Введите любое число", 100);
-    }
-
-    arr = inputNum.split("");
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === " ") {
-            arr.splice(i, 1);
-            i = i - 1;
+const logger = function (arr) {
+    let date = new Date();
+    let todayDay = date.getDay();
+    // console.log(todayDay);
+    for (day of arr) {
+        if (day === "saturday" || day === "sunday") {
+            console.log("%c" + day, "font-style: italic;");
+        } else if (arr.indexOf(day) === todayDay - 1) {
+            console.log("%c" + day, "font-weight: bold;");
+        } else {
+            console.log(day);
         }
     }
-
-    return Number((inputNum = arr.join("")));
 };
 
-console.log(getNumber());
+logger(week);
+
+// выводить нужно на страницу html
+// реализовать
